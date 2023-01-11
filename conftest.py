10831031ee966a619d9b4e88a8a1b05d5cd43afe
My Name <myemail@example.com>
2023-01-11 10:51:06 +0100

    scope add

--- conftest.py
+++ conftest.py
@@ -4,3 +4,10 @@ def set_up():
     print("You are login!")
     yield
     print("Exit from system!")
+
+
+@pytest.fixture
+def some(scope = 'module'):
+    print("Start!")
+    yield
+    print("End!")
--- test_mail2.py
+++ test_mail2.py
@@ -3,10 +3,10 @@ import pytest
 #deistvie do
 
 #parametr kotoryi peredaem
-def test_sending_mail3(set_up):
+def test_sending_mail3(set_up,some):
     print("The letter sended")
 
-def test_sending_mail4(set_up):
+def test_sending_mail4(set_up,some):
     print("The letter sended")
 
 
